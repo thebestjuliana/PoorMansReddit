516a59bbb05b2fd8e74890d3a945f3f95ea5c07e
thebestjuliana <76972376+thebestjuliana@users.noreply.github.com>
2021-03-01 16:20:05 -0500

    initial commit

---
+++ Lab27_PoorMansReddit/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Lab27_PoorMansReddit.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab27_PoorMansReddit.Controllers
+{
+    public class HomeController : Controller
+        
+    {
+        private readonly ILogger<HomeController> _logger;
+        private RedditDAL redditDAL = new RedditDAL();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            SubReddit sub = redditDAL.ConvertToSubReddit();
+            return View(sub);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab27_PoorMansReddit/Models/RedditDAL.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Lab27_PoorMansReddit.Models
+{
+    public class RedditDAL
+    {
+
+        public string GetData()
+        {
+            string url = @$"https://www.reddit.com/r/aww/.json";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string JSON = rd.ReadToEnd();
+
+            return JSON;
+        }
+
+        public SubReddit ConvertToSubReddit()
+        {
+            string subredditData = GetData();
+            SubReddit subreddit = JsonConvert.DeserializeObject<SubReddit>(subredditData);
+            return subreddit;
+        }
+    }
+}
